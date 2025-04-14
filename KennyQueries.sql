855f21b441c5806c8ac77e505c64ad1aa2ba1ac5
ktran1121 <182476281+ktran1121@users.noreply.github.com>
2025-04-13 22:34:10 -0700

    query 4

--- KennyQueries.sql
+++ KennyQueries.sql
@@ -1,5 +1,9 @@
 --Kenny Khoi Tran
+
+
 --Unoptimized Queries
+
+--Query 1:
 -- Count how many times each event type occurred and calculate the total number of direct and indirect injuries and deaths, along with total property and crop damage. 
 --The results are grouped by event type and sorted by the number of events in descending order.
 --Unoptimized: This query will process 5 MB when run.
@@ -21,7 +25,7 @@ ORDER BY
   event_count DESC;
 
 
-
+--Query 2:
 --Top 10 tornadoes with the longest length of the path in miles in descending order.
 --This query will process 2.8 MB when run.
 
@@ -42,6 +46,7 @@ ORDER BY
 LIMIT 10;
 
 
+--Query 3:
 --Top 10 tornadoes with the highest fatality count in descending order.
 --This query will process 2.8 MB when run.
 
@@ -59,6 +64,29 @@ WHERE
   fatality_count IS NOT NULL
 ORDER BY
   fatality_count DESC
+LIMIT 10;
+
+
+
+--Query 4:
+--Top 10 states with the highest recorded size of hail in descending order.
+--This query will process 321.6 KB when run.
+
+SELECT
+  MIN(h.timestamp) AS hail_timestamp,
+  h.state,
+  h.county,
+  h.location,
+  h.size / 100.0 AS hail_size_inches,  --Size in 1/100 of an Inch (175 = 1.75in)
+  STRING_AGG(h.comments, ' | ') AS all_comments  -- Concatenate all comments for the event since there are duplicate events with different comments
+FROM
+  `bigquery-public-data.noaa_historic_severe_storms.hail_reports` h
+WHERE
+  h.size IS NOT NULL
+GROUP BY
+  h.state, h.county, h.location, hail_size_inches
+ORDER BY
+  hail_size_inches DESC
 LIMIT 10;
 
 
