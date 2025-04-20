29d1106383849b6bc4b25e517008e8fcbb050562
cjin1510 <125084265+cjin1510@users.noreply.github.com>
2025-04-20 00:08:33 -0700

    Update JQ-SQL.sql

--- JQ-SQL.sql
+++ JQ-SQL.sql
@@ -1,11 +1,16 @@
 -- Jin Chen 
 -- Unoptimized Queries
--- This query shows the Top 10 states with the Total Amount of casulaties by doing direct + indirect injuries/deaths from 2010 - 2024. 
--- changing to distinct so it counts the events and casualties once
+-- This query shows the Top 10 states with the Total Amount of casulaties by doing direct + indirect injuries/deaths 
+-- from 2010 - 2024. 
+-- Changing to distinct so it counts the events and casualties once.
+-- Results will have the event_count and total_casualties which is UNIQUE so there are no duplicates of deaths or injuries
+-- or duplicates of event_id. The total_casualties will be the sum of schemas from storms 2022-2024 in respect to the state.
+-- Bytes processed 8.63 MB, Bytes billed 20 MB
+
 SELECT   
   s.state,
   COUNT(DISTINCT s.event_id) AS event_count,
-  SUM(DISTINCT
+  SUM(DISTINCT                       -- Changing to DISTINCT to make sure there are no duplicates 
     IFNULL(s.injuries_direct, 0)
   + IFNULL(s.injuries_indirect, 0)
     + IFNULL(s.deaths_direct, 0)
