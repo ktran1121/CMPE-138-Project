a1dd871d906b3e22874b2994c904fc62d75fe52a
cjin1510 <125084265+cjin1510@users.noreply.github.com>
2025-04-19 23:50:47 -0700

    Update JQ-SQL.sql

--- JQ-SQL.sql
+++ JQ-SQL.sql
@@ -1,22 +1,24 @@
 -- Jin Chen 
 -- Unoptimized Queries
 -- This query shows the Top 10 states with the Total Amount of casulaties by doing direct + indirect injuries/deaths from 2010 - 2024. 
-
-SELECT
-  state,
-  COUNT(*) AS event_count,
-  SUM(
-    IFNULL(injuries_direct, 0) 
-   + IFNULL(injuries_indirect, 0)
-     + IFNULL(deaths_direct, 0)
-    + IFNULL(deaths_indirect, 0)
+-- changing to distinct so it counts the events and casualties once
+SELECT   
+  s.state,
+  COUNT(DISTINCT s.event_id) AS event_count,
+  SUM(DISTINCT
+    IFNULL(s.injuries_direct, 0)
+  + IFNULL(s.injuries_indirect, 0)
+    + IFNULL(s.deaths_direct, 0)
+    + IFNULL(s.deaths_indirect, 0)
   ) AS total_casualties
 FROM
-  `bigquery-public-data.noaa_historic_severe_storms.storms_*`
+  `bigquery-public-data.noaa_historic_severe_storms.storms_*` AS s
+JOIN
+  `bigquery-public-data.noaa_historic_severe_storms.nws_forecast_regions` AS r  ON s.wfo = r.cwa
 WHERE
-  _TABLE_SUFFIX BETWEEN '2010' AND '2024'  -- using storms_* thus we can use _TABLE_SUFFIX to show which table each row comes from
+  _TABLE_SUFFIX IN ('2023', '2024') -- using storms_* thus we can use _TABLE_SUFFIX to show which table each row comes from
 GROUP BY
-  state
+  s.state
 ORDER BY
   total_casualties DESC
 LIMIT
