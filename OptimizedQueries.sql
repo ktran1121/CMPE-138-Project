431c091439fadadc5e13befb2d3661bf3e252f97
cjin1510 <125084265+cjin1510@users.noreply.github.com>
2025-04-28 16:50:47 -0700

    Update OptimizedQueries.sql

--- OptimizedQueries.sql
+++ OptimizedQueries.sql
@@ -48,7 +48,32 @@ ORDER BY
 LIMIT
   20;
 
-
-
+-- Jin
+-- Bytes processed 3.32 MB
+-- Bytes billed 20 MB
+-- Slot milliseconds 340
+-- This query is an optimized version from query 1 
+WITH only_order_items AS (  -- using Common table expression to create a subquery to scan only what i need
+  SELECT
+    product_id,
+    sale_price -- selecting only what i need from query 1 so it doesn't scan everything from order items
+  FROM
+    `bigquery-public-data.thelook_ecommerce.order_items`
+)
+SELECT
+  p.category                      AS product_category,
+  ROUND(SUM(oi.sale_price), 2)    AS total_sales_revenue,
+  COUNT(*)                        AS total_units_sold
+FROM
+  only_order_items AS oi
+JOIN
+  `bigquery-public-data.thelook_ecommerce.products` AS p
+  ON oi.product_id = p.id
+GROUP BY
+  p.category
+ORDER BY
+  total_sales_revenue DESC
+LIMIT
+  10;
 
 --add more later
