a56c6f432a1aa4804f1b5c87289d46e9da909fd1
ktran1121 <182476281+ktran1121@users.noreply.github.com>
2025-04-28 22:01:52 -0700

    Update OptimizedQueries.sql

--- OptimizedQueries.sql
+++ OptimizedQueries.sql
@@ -4,47 +4,37 @@
 --Top 20 best-selling products in the “Active” category by total items sold in descending order. (Optimized)
 --Kenny Khoi Tran
 --(415 + 395 + 474 + 392 + 399)/5 = 415 ms
---This query will process 4.72 MB when run.
+--This query will process 4.75 MB when run.
 
-WITH active_products AS (
-  SELECT
-    id AS product_id,
-    name AS product_name,
-    category
-  FROM
-    `bigquery-public-data.thelook_ecommerce.products`
-  WHERE
-    category = "Active"
-),
-product_sales AS (
+WITH active_sales AS (
   SELECT
     oi.product_id,
+    p.category,
+    p.name          AS product_name,
     oi.sale_price
   FROM
-    `bigquery-public-data.thelook_ecommerce.order_items` AS oi
+    `bigquery-public-data.thelook_ecommerce.products`   AS p
   JOIN
-    active_products AS ap
-    ON oi.product_id = ap.product_id
+    `bigquery-public-data.thelook_ecommerce.order_items` AS oi
+    ON p.id = oi.product_id
+  WHERE
+    p.category = "Active"
 )
-
 SELECT
-  ps.product_id,
-  ap.category,
-  ap.product_name,
-  ROUND(SUM(ps.sale_price), 2) AS total_sales,
-  COUNT(*) AS total_items_sold
+  product_id,
+  category,
+  product_name,
+  ROUND(SUM(sale_price), 2) AS total_sales,
+  COUNT(1)              AS total_items_sold
 FROM
-  product_sales AS ps
-JOIN
-  active_products AS ap
-  ON ps.product_id = ap.product_id
+  active_sales
 GROUP BY
-  ps.product_id,
-  ap.category,
-  ap.product_name
+  product_id,
+  category,
+  product_name
 ORDER BY
   total_items_sold DESC,
-  total_sales DESC
+  total_sales      DESC
 LIMIT
   20;
 
